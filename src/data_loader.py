9802eb271314a460241723a65235e2ddedd27bb1
Sverre Nystad <sverrnys@stud.ntnu.no>
2024-03-04 23:25:49 +0100

    feat: add data loading functions

---
+++ src/data_loader.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def _func(X: np.ndarray) -> np.ndarray:
+    """
+    The data generating function.
+    Do not modify this function.
+    """
+    return 0.3 * X[:, 0] + 0.6 * X[:, 1] ** 2
+
+
+def _noisy_func(X: np.ndarray, epsilon: float = 0.075) -> np.ndarray:
+    """
+    Add Gaussian noise to the data generating function.
+    Do not modify this function.
+    """
+    return _func(X) + np.random.randn(len(X)) * epsilon
+
+
+def get_data(n_train: int, n_test: int) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Generating training and test data for
+    training and testing the neural network.
+    Do not modify this function.
+    """
+    X_train = np.random.rand(n_train, 2) * 2 - 1
+    y_train = _noisy_func(X_train)
+    X_test = np.random.rand(n_test, 2) * 2 - 1
+    y_test = _noisy_func(X_test)
+
+    return X_train, y_train, X_test, y_test

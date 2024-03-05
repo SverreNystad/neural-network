ac00605dc0f8ddf9db68b85e97e1b27e43b5f7df
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-03-05 02:10:51 +0100

    test: Add unit tests for neural network output shape and input validation

---
+++ test/test_assignment7.py
@@ -0,0 +1,26 @@
+from src.data_loader import get_data
+from src.neural_network import Layer, NeuralNetwork, Sigmoid
+import numpy as np
+
+np.random.seed(0)
+
+
+def test_2_1_1_network_output_shape():
+    X_train, y_train, X_test, y_test = get_data(n_train=280, n_test=120)
+    layers = [Layer(1, Sigmoid)]
+    network = NeuralNetwork(2, layers, 1)
+    network.train(X_train, y_train)
+    y_pred = network.predict(X_test)
+    assert y_pred.shape == y_test.shape
+
+
+def test_wrong_input_num():
+    X_train, y_train, X_test, y_test = get_data(n_train=280, n_test=120)
+    layers = [Layer(1, Sigmoid)]
+    network = NeuralNetwork(3, layers, 1)
+    try:
+        network.predict(X_train)
+    except ValueError:
+        assert True
+    else:
+        assert False

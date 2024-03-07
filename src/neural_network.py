92c73ee04480d66d2f88a22f427e30e7217af0ba
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-03-07 02:04:38 +0100

    refactor: extract loss functions

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,39 +1,6 @@
 import numpy as np
-from typing import Union
-from abc import ABC, abstractmethod
 
 from src.activation import Activation
-
-
-class Loss(ABC):
-    """
-    The loss function or cost function is a function that maps an event or values of one or more variables onto a real number intuitively representing some "cost" associated with the event.
-    """
-
-    @abstractmethod
-    def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
-        """
-        Computes the loss between the true labels and the predicted labels
-        """
-        pass
-
-
-class MeanSquaredError(Loss):
-    def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
-        """
-        Computes the mean squared error
-
-        Formula:
-        mse = (1/n) * Σ(y_true - y_pred)^2
-
-        Args:
-            y_true (np.ndarray): true labels
-            y_pred (np.ndarray): predicted labels
-
-        Returns:
-            np.ndarray: mean squared error
-        """
-        return np.mean((y_true - y_pred) ** 2)
 
 
 class HyperParameters:

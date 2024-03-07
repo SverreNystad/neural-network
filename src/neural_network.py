a4334e81a57a57eccd8a6241555325dc05b9f2a5
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-03-07 02:20:08 +0100

    refactor: Change validation for output array size to not use assert but throw valueError

--- src/neural_network.py
+++ src/neural_network.py
@@ -165,5 +165,8 @@ class NeuralNetwork:
             activation = layer.forward(activation)
 
         # The output tensor of the last hidden layer is the input tensor to the output layer
-        assert x.shape[1] == self.output_layer_size
+        if x.shape[1] != self.output_layer_size:
+            raise ValueError(
+                f"Output array must have {self.output_layer_size} features, but got {x.shape[1]}"
+            )
         return x

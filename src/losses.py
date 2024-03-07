374e95a1071e10cecf726244e71444f1b5415c4a
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-03-07 02:10:22 +0100

    feat: add MeanAbsoluteError class to losses.py

--- src/losses.py
+++ src/losses.py
@@ -49,3 +49,21 @@ class RootMeanSquaredError(Loss):
             np.ndarray: root mean squared error
         """
         return np.sqrt(np.mean((y_true - y_pred) ** 2))
+
+
+class MeanAbsoluteError(Loss):
+    def __call__(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        """
+        Computes the mean absolute error
+
+        Formula:
+        mae = (1/n) * Σ|y_true - y_pred|
+
+        Args:
+            y_true (np.ndarray): true labels
+            y_pred (np.ndarray): predicted labels
+
+        Returns:
+            np.ndarray: mean absolute error
+        """
+        return np.mean(np.abs(y_true - y_pred))

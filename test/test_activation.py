6090fab718bada2f27d31035af857a24ace2bce7
Sverre Nystad <89105607+SverreNystad@users.noreply.github.com>
2024-03-09 16:53:48 +0100

    test: add test cases for loss functions

--- test/test_activation.py
+++ test/test_activation.py
@@ -1,6 +1,9 @@
+import numpy as np
 import pytest
 from src.data_loader import get_data
+from src.losses import Loss, MeanSquaredError
 from src.neural_network import Sigmoid, Activation, NeuralNetwork
+
 
 def test_get_data():
     X_train, y_train, X_test, y_test = get_data(n_train=280, n_test=120)
@@ -17,12 +20,28 @@ def test_activation():
     activated = activation_function(X_train)
     assert activated.shape == X_train.shape
 
-# def test_prediction():
-#     X_train, y_train, X_test, y_test = get_data(n_train=280, n_test=120)
-#     activation_function: Activation = Sigmoid()
-    
-#     nn: NeuralNetwork = NeuralNetwork(activation_function)
-#     nn.train(X_train, y_train)
 
-#     predictions = nn.forward(X_test)
-#     assert predictions.shape == y_test.shape
+def test_zero_loss():
+    loss_function: Loss = MeanSquaredError()
+    y_prediction = np.array([0, 1, 0, 1, 0])
+    y_true = np.array([0, 1, 0, 1, 0])
+    loss = loss_function(y_true, y_prediction)
+    derived = loss_function.derived(y_true, y_prediction)
+    expected_loss = 0
+    expected_derived = np.array([0, 0, 0, 0, 0])
+
+    assert np.isclose(loss, expected_loss)
+    assert np.isclose(derived, expected_derived).all()
+
+
+def test_some_loss():
+    loss_function: Loss = MeanSquaredError()
+    y_true = np.array([3.0, -0.5, 2.0, 7.0], dtype=float)
+    y_prediction = np.array([2.5, 0.0, 2.0, 8.0], dtype=float)
+    loss = loss_function(y_true, y_prediction)
+    derived = loss_function.derived(y_true, y_prediction)
+    expected_loss = 0.375
+    expected_derived = np.array([-1.0, 1.0, 0.0, 2.0])
+
+    assert np.isclose(loss, expected_loss)
+    assert np.isclose(derived, expected_derived).all()

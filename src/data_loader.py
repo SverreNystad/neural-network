16288c67c6718d3a7a9875519c273b24d2d24746
SverreNystad <sverrenystad@gmail.com>
2024-05-27 00:44:16 +0200

    refactor: Update data_loader.py to improve data generation

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,11 +1,19 @@
 import numpy as np
 
-def _func(X: np.ndarray) -> np.ndarray:
+
+def get_data(
+    n_train: int, n_test: int
+) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
-    The data generating function.
+    Generating training and test data for
+    training and testing the neural network.
     Do not modify this function.
     """
-    return 0.3 * X[:, 0] + 0.6 * X[:, 1] ** 2
+    X_train = np.random.rand(n_train, 2) * 2 - 1
+    y_train = _noisy_func(X_train)
+    X_test = np.random.rand(n_test, 2) * 2 - 1
+    y_test = _noisy_func(X_test)
+    return X_train, y_train, X_test, y_test
 
 
 def _noisy_func(X: np.ndarray, epsilon: float = 0.075) -> np.ndarray:
@@ -16,15 +24,9 @@ def _noisy_func(X: np.ndarray, epsilon: float = 0.075) -> np.ndarray:
     return _func(X) + np.random.randn(len(X)) * epsilon
 
 
-def get_data(n_train: int, n_test: int) -> tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+def _func(X: np.ndarray) -> np.ndarray:
     """
-    Generating training and test data for
-    training and testing the neural network.
+    The data generating function.
     Do not modify this function.
     """
-    X_train = np.random.rand(n_train, 2) * 2 - 1
-    y_train = _noisy_func(X_train)
-    X_test = np.random.rand(n_test, 2) * 2 - 1
-    y_test = _noisy_func(X_test)
-
-    return X_train, y_train, X_test, y_test
+    return 0.3 * X[:, 0] + 0.6 * X[:, 1] ** 2
